7f4f03b8ce7dfb3f6cf57b6fe56dd7019e61e9fc
Connor Davis <19422@kloofhigh.co.za>
2021-03-16 05:41:37 +0200

    fix up folder structure

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './app.css'
 
 import { HashRouter, Route, Switch } from 'react-router-dom'
 
-import { Header } from './components/header/header'
-import { NotFound } from './pages/404/NotFound'
-import Syllektions from './pages/syllektions/syllektions'
-import Syllektors from './pages/syllektors/syllektors'
+import { Header } from './app/components/header/header'
+import { NotFound } from './app/pages/404/NotFound'
+import Syllektions from './app/pages/syllektions/syllektions'
+import Syllektors from './app/pages/syllektors/syllektors'
 import { useEffect } from 'react'
+
+/**
+ * Momentum: Momentum of an object is the product of the mass and velocity of a moving body.
+ * Newtons Second Law Of Motion: The net or resultant force acting on an object is equal to the rate of change in momentum.
+ * The Conservation Of Linear Momentum: If two objects collide, then the total momentum before and after the collision will be the same if there is no external force acting on the colliding objects.
+ */
+
 
 function App() {
     useEffect(() => {}, [])
